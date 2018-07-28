6f51da48f07d0eb3d284d3060183d9cf0010087c
pisey <pisey@marinax.biz>
2018-07-28 11:45:17 +0700

    - COMPONENTS AND .VUE FILES

---
+++ vue-webpack/src/Input.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <input type="text" name="" id="" v-model="message">
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "app-input",
+        data() {
+            return{
+                message : '123'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-webpack/src/Message.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>This is a message</h1>
+        <app-input></app-input>
+    </div>
+</template>
+
+<script>
+
+    import Input from './Input.vue';
+
+    export default {
+        name: "app-message",
+        components:{
+            'app-input': Input
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
